b9a8369ac14a623a5c376de5f2943f3cf5a1c165
Adam Mechaeil <mechaeil66@yahoo.com>
2026-02-04 06:14:08 +0530

    hardcoded cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [process.env.CLIENT_URL || "http://localhost:3000"], // Specific origin required for credentials
+    origin: [
+      process.env.CLIENT_URL as string,
+      "https://lms-client-jry7.onrender.com",
+      "http://localhost:3000",
+    ], // Specific origin required for credentials
     methods: ["GET", "POST"],
     credentials: true,
   },
